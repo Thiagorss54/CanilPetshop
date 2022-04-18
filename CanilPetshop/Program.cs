4e02a425a21b58e1ccbe90d931ff2fac06f9c22d
Thiagorss54 <thiagorss54@gmail.com>
2022-04-18 09:32:01 -0300

    programa finalizado

--- CanilPetshop/Program.cs
+++ CanilPetshop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,45 +13,85 @@ namespace CanilPetshop
     {
         static void Main(string[] args)
         {
-            DateTime data;
-            int qtdCaesPequenos;
-            int qtdCaesGrandes;
-
-            Console.WriteLine("Digite a entrada no formato <dd/mm/yyyy> <quantidade de cães pequenos> <quantidade de cães grandes>");
-            string[] entrada = Console.ReadLine().Split(' ');
-            data = DateTime.Parse(entrada[0]);
-            qtdCaesPequenos = int.Parse(entrada[1]);
-            qtdCaesGrandes = int.Parse(entrada[2]);
-
-            List<Petshop> petshops = new List<Petshop>();
-
-            petshops.Add(new MeuCaninoFeliz());
-            petshops.Add(new VaiRex());
-            petshops.Add(new Petshop("ChowChagas", 0.8, 30, 45));
-
-            Petshop petshopEscolhido = petshops.First();
-            double menorPreco = petshopEscolhido.CalcularPreco(data, qtdCaesPequenos, qtdCaesGrandes);
-
-            foreach (Petshop petshop in petshops)
+            bool fecharPrograma = false;
+            while (!fecharPrograma)
             {
-                double precoTotal = petshop.CalcularPreco(data, qtdCaesPequenos, qtdCaesGrandes);
-                if (precoTotal < menorPreco)
+                Console.Clear();
+                TelaInicial();
+                string[] entrada = Console.ReadLine().Split(' ');
+                Console.WriteLine();
+                EncontrarMelhorPetshop(entrada);
+                Console.WriteLine();
+                Console.Write("Deseja rodar o programa novamente?(S/N): ");
+                char resposta = char.Parse(Console.ReadLine());
+                if(resposta !='s' && resposta != 'S')
                 {
-                    petshopEscolhido = petshop;
-                    menorPreco = precoTotal;
+                    fecharPrograma=true;
                 }
-                else if (precoTotal == menorPreco && petshop.Distancia < petshopEscolhido.Distancia)
+            }
+
+        }
+        static void TelaInicial()
+        {
+            Console.WriteLine("---------------------------------- ENCONTRAR O MELHOR PETSHOP --------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("O formato da entrada é <dd/mm/yyyy> <quantidade de cães pequenos> <quantidade de cães grandes>");
+            Console.WriteLine("Ex: 17/04/2022 3 7");
+            Console.WriteLine();
+            Console.Write("Digite a entrada: ");
+        }
+        static void EncontrarMelhorPetshop(string[] entrada)
+        {
+            try
+            {
+                DateTime data = DateTime.Parse(entrada[0]); 
+                int qtdCaesPequenos = int.Parse(entrada[1]); 
+                int qtdCaesGrandes = int.Parse(entrada[2]); 
+
+
+
+                List<Petshop> petshops = new List<Petshop>();
+
+                petshops.Add(new MeuCaninoFeliz());
+                petshops.Add(new VaiRex());
+                petshops.Add(new Petshop("ChowChagas", 0.8, 30, 45));
+
+                Petshop petshopEscolhido = petshops.First();
+                double menorPreco = petshopEscolhido.CalcularPreco(data, qtdCaesPequenos, qtdCaesGrandes);
+
+                foreach (Petshop petshop in petshops)
                 {
-                    petshopEscolhido = petshop;
-                    menorPreco = precoTotal;
+                    double precoTotal = petshop.CalcularPreco(data, qtdCaesPequenos, qtdCaesGrandes);
+                    if (precoTotal < menorPreco)
+                    {
+                        petshopEscolhido = petshop;
+                        menorPreco = precoTotal;
+                    }
+                    else if (precoTotal == menorPreco && petshop.Distancia < petshopEscolhido.Distancia)
+                    {
+                        petshopEscolhido = petshop;
+                        menorPreco = precoTotal;
+                    }
+                    
                 }
-                Console.WriteLine(petshop.Nome + " " + precoTotal);
+                
+                Console.WriteLine($"O melhor petshop para levar os cães é o {petshopEscolhido.Nome} e o preço total dos banhos é de R${menorPreco.ToString("F2",CultureInfo.InvariantCulture)}.");
+                Console.WriteLine();
+
 
             }
-            Console.WriteLine();
-            Console.WriteLine("O melhor petshop para levar os cães é o " + petshopEscolhido.Nome);
-
-
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Erro: Formato inválido. O formato de entrada não está de acordo com o formato padrão.");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Erro: Faltou algum valor de entrada.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
         }
     }
 }
